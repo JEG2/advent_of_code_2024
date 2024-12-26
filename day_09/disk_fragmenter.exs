f517d10b4ac1bd140b3f89a15b0632b653f9cd3f
James Edward Gray II <james@graysoftinc.com>
2024-12-26 17:54:10 -0600

    Day 9 Part 2

--- day_09/disk_fragmenter.exs
+++ day_09/disk_fragmenter.exs
@@ -3,8 +3,8 @@ defmodule DiskFragmenter do
 
   def solve(args) do
     case OptionParser.parse!(args, strict: [part_2: :boolean]) do
-      # {[part_2: true], [input_path]} ->
-      #   solve_part_2(input_path)
+      {[part_2: true], [input_path]} ->
+        solve_part_2(input_path)
 
       {_part_1, [input_path]} ->
         solve_part_1(input_path)
@@ -41,7 +41,7 @@ defmodule DiskFragmenter do
         |> Map.put_new(free, Map.fetch!(blocks, cursor))
         |> Map.delete(cursor),
         Enum.find((free + 1)..cursor//1, &(not Map.has_key?(blocks, &1))),
-        Enum.find((cursor - 1)..0//-1, &Map.has_key?(blocks, &1))
+        reduce_cursor(blocks, cursor)
       }
     end)
     |> Enum.find(fn {_blocks, free, cursor} -> free >= cursor end)
@@ -50,8 +50,67 @@ defmodule DiskFragmenter do
     |> Enum.reduce(0, fn {i, id}, acc -> acc + i * id end)
   end
 
-  # def solve_part_2(input_path) do
-  # end
+  def solve_part_2(input_path) do
+    input_path
+    |> File.read!()
+    |> String.trim()
+    |> String.graphemes()
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.with_index()
+    |> Enum.reduce({%{}, [], 0}, fn
+      {0, _i}, acc ->
+        acc
+
+      {len, i}, {blocks, free, cursor} when Integer.is_even(i) ->
+        {Map.put(blocks, cursor, {div(i, 2), len}), free, cursor + len}
+
+      {len, i}, {blocks, free, cursor} when Integer.is_odd(i) ->
+        {blocks, [{cursor, len} | free], cursor + len}
+    end)
+    |> then(fn {blocks, free, cursor} ->
+      {blocks, Enum.reverse(free), reduce_cursor(blocks, cursor)}
+    end)
+    |> Stream.iterate(fn {blocks, free, cursor} ->
+      {id, len} = Map.fetch!(blocks, cursor)
+
+      {blocks, free} =
+        case Enum.find_index(free, fn {i, l} -> i < cursor and len <= l end) do
+          j when is_integer(j) ->
+            {i, l} = Enum.at(free, j)
+
+            blocks =
+              blocks
+              |> Map.put_new(i, {id, len})
+              |> Map.delete(cursor)
+
+            free =
+              if len == l do
+                List.delete_at(free, j)
+              else
+                List.replace_at(free, j, {i + len, l - len})
+              end
+
+            {blocks, free}
+
+          nil ->
+            {blocks, free}
+        end
+
+      {blocks, free, reduce_cursor(blocks, cursor)}
+    end)
+    |> Enum.find(fn {_blocks, _free, cursor} -> is_nil(cursor) end)
+    |> elem(0)
+    |> Enum.sort()
+    |> Enum.reduce(0, fn {i, {id, len}}, acc ->
+      Enum.reduce(0..(len - 1), acc, fn offset, acc ->
+        acc + (i + offset) * id
+      end)
+    end)
+  end
+
+  defp reduce_cursor(blocks, cursor) do
+    Enum.find((cursor - 1)..0//-1, &Map.has_key?(blocks, &1))
+  end
 end
 
 System.argv()
